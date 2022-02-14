70e58d9b721519341aa0b7e8e693c3cf2aa98cdd
Akash Das <mfs.akash@gmail.com>
2022-02-14 17:03:08 +0530

    loops demo

---
+++ Loops.java
@@ -0,0 +1,8 @@
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("The first ten natural numbers are: ");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
