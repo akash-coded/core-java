b3ab2c82aa20a552e3c816c0c9a39ee72c223df6
Akash Das <mfs.akash@gmail.com>
2022-02-14 13:04:14 +0530

    method overriding demo

---
+++ MethodOverriding.java
@@ -0,0 +1,33 @@
+class Base {
+    void add(int x, int y) {
+        System.out.println("Addition results in " + (x + y));
+    }
+}
+
+class Derived1 extends Base {
+    // @Override
+    void add(int x, int y, int z) {
+        System.out.println("Sum of three numbers is " + (x + y + z));
+    }
+}
+
+class Derived2 extends Base {
+    @Override
+    void add(int x, int y) {
+        super.add(x, y);
+        System.out.println("Sum = " + (x + y));
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Base b = new Base();
+        Derived1 d1 = new Derived1();
+        Derived2 d2 = new Derived2();
+
+        b.add(2, 3);
+        d1.add(2, 3);
+        d1.add(2, 3, 4);
+        d2.add(2, 3);
+    }
+}
