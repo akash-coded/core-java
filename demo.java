707326464b1f8813be7f799f4e1e45ec89fd83f1
Akash Das <mfs.akash@gmail.com>
2022-02-14 17:00:18 +0530

    java functions demo

--- demo.java
+++
@@ -1,5 +0,0 @@
-class Demo {
-    public static void main(String[] args) {
-        System.out.println("Git and GitHub demo");
-    }
-}
--- third.java
+++
@@ -1,5 +0,0 @@
-class Third {
-    public static void main(String[] args) {
-
-    }
-}
