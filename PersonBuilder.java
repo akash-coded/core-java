915606ad3ab18c9e8e4f13d214a7ecb24f412599
Akash Das <mfs.akash@gmail.com>
2022-02-14 17:04:28 +0530

    oops - classes and objects

---
+++ PersonBuilder.java
@@ -0,0 +1,22 @@
+class Person {
+    int id; // instance variable
+    String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+public class PersonBuilder {
+    private static Person buildPerson(int id, String name) { // factory method
+        return new Person(id, name);
+    }
+
+    public static void main(String[] args) {
+        int id = 23;
+        String name = "John";
+        Person person = null;
+        person = buildPerson(id, name);
+    }
+}
