448f1b5d67eab16f72cb3b75c99c785711fff88d
Akash Das <mfs.akash@gmail.com>
2022-02-25 17:11:42 +0530

    thread synchronization

---
+++ multithreading/StaticSynchronization.java
@@ -0,0 +1,38 @@
+package multithreading;
+
+class ReciteTable {
+    static synchronized void printTable(int n) {
+        System.out.println(Thread.currentThread().getName() + " has locked the synchronized block");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " is releasing the lock");
+
+        System.out.println("Non-synchronized line printed by " +
+                Thread.currentThread().getName());
+    }
+}
+
+class TestStaticSynchronization extends Thread {
+    @Override
+    public void run() {
+        ReciteTable.printTable(5);
+    }
+}
+
+public class StaticSynchronization {
+    public static void main(String[] args) {
+        TestStaticSynchronization thread1 = new TestStaticSynchronization();
+        TestStaticSynchronization thread2 = new TestStaticSynchronization();
+        TestStaticSynchronization thread3 = new TestStaticSynchronization();
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
---
+++ multithreading/SynchronizedMethodDemo.java
@@ -0,0 +1,46 @@
+package multithreading;
+
+class TablePrinter {
+    synchronized void printTable(int n) {
+        System.out.println(Thread.currentThread().getName() + " has locked the synchronized block");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " is releasing the lock");
+
+        System.out.println("Non-synchronized line printed by " +
+                Thread.currentThread().getName());
+    }
+}
+
+class TestSynchronizedMethod extends Thread {
+    TablePrinter table;
+
+    TestSynchronizedMethod(TablePrinter table) {
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        table.printTable(5);
+    }
+}
+
+public class SynchronizedMethodDemo {
+    public static void main(String[] args) {
+        TablePrinter table = new TablePrinter();
+
+        TestSynchronizedMethod thread1 = new TestSynchronizedMethod(table);
+        TestSynchronizedMethod thread2 = new TestSynchronizedMethod(table);
+        TestSynchronizedMethod thread3 = new TestSynchronizedMethod(table);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
