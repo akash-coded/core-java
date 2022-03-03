a57bb5b0297d569f52c057de6aaf320938a27429
Akash Das <mfs.akash@gmail.com>
2022-03-03 18:03:45 +0530

    added comments to collections demo program

--- collections/ArrayListDemo.java
+++ collections/ArrayListDemo.java
@@ -7,48 +7,49 @@ import java.util.function.Consumer;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
+        // Creating a collection
         ArrayList<Integer> list1 = new ArrayList<>();
+
+        // Adding elements to a collection
         list1.add(5);
         list1.add(1);
         list1.add(7);
         list1.add(10);
 
+        // Iterating over a collection
+        System.out.println("Original list (List-1):: ");
         Iterator<Integer> itr = list1.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next());
         }
         System.out.println();
 
+        // Accessing an element of a collection
+        System.out.print("Third element of List-1 = ");
+        System.out.println(list1.get(2)); // retrieves the element at index-2
+        System.out.println();
+
+        // Removing an element from a collection
         Integer elementToBeRemoved = 10;
-        list1.remove(elementToBeRemoved);
+        list1.remove(elementToBeRemoved); // using Integer argument removes that object
+        System.out.println("List-1 after removal of 10  - ");
         Iterator<Integer> itr2 = list1.iterator();
         while (itr2.hasNext()) {
             System.out.println(itr2.next());
         }
         System.out.println();
 
-        System.out.println(list1.get(2));
-        System.out.println();
-
-        System.out.println("Cloned list before removal:: ");
-        ArrayList<Integer> list2 = (ArrayList<Integer>) list1.clone();
-        Iterator<Integer> itr3 = list2.iterator();
-        while (itr3.hasNext()) {
-            System.out.println(itr3.next());
-        }
-        System.out.println();
-
-        list1.remove(0); // removes the element at index-0
-        System.out.println("After removal - ");
-        System.out.println("List-1:: ");
+        // Removing an element at a given index from a collection
+        list1.remove(0); // using int argument removes the element at that index
+        System.out.println("List-1 after removal of the first element:: ");
         for (Integer ele : list1) {
             System.out.println(ele);
         }
         System.out.println();
 
+        // Modifying an element of a collection
         list1.set(1, 100);
-        System.out.println("After modification - ");
-        System.out.println("List-1:: ");
+        System.out.println("List-1 after modification:: ");
         Consumer<Integer> consumer = new Consumer<>() {
             @Override
             public void accept(Integer ele) {
@@ -58,14 +59,29 @@ public class ArrayListDemo {
         list1.forEach(consumer);
         System.out.println();
 
+        // Copying a collection
+        System.out.println("Cloned list (List-2):: ");
+        ArrayList<Integer> list2 = (ArrayList<Integer>) list1.clone();
+        Iterator<Integer> itr3 = list2.iterator();
+        while (itr3.hasNext()) {
+            System.out.println(itr3.next());
+        }
+        System.out.println();
+
+        // Sorting a collection in ascending order
         Collections.sort(list2);
+        System.out.println("List-2 after sorting  in ascending order:: ");
         list2.forEach(ele -> System.out.println(ele));
         System.out.println();
 
+        // Sorting a collection in descending order
+        System.out.println("List-2 after sorting in descending order:: ");
         Collections.sort(list2, Collections.reverseOrder());
         list2.forEach(System.out::println);
         System.out.println();
 
+        // Displaying a collection
+        System.out.println("Dsiplaying List-2:: ");
         System.out.println(list2);
     }
 }
