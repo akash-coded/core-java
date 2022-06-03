e20ce2f1c7d512ba178bd53bafec8db306f3fa75
Akash Das <mfs.akash@gmail.com>
2022-06-03 10:08:11 +0530

    added code demo for output formatting in Java

---
+++ src/language_basics/io/OutputFormattingDemo.java
@@ -0,0 +1,20 @@
+package src.language_basics.io;
+
+public class OutputFormattingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Plain message");
+        System.out.println("Message with data:: " + 10);
+        System.out.println("Message with multiple data. Data1 = " + 10 + ", Data2 = " + 20);
+        System.out.println();
+
+        System.out.println(String.format("Message with data (using formatted string):: %d", 10));
+        System.out.println(
+                String.format("Message with multiple data. (using formatted string):: Data1 = %d, Data2 = %d", 10, 20));
+        System.out.println();
+
+        System.out.printf("Message with data (using printf()):: %d%n", 10);
+        System.out.printf("Message with multiple data. (using printf()):: Data1 = %d, Data2 = %d", 10, 20);
+    }
+
+}
